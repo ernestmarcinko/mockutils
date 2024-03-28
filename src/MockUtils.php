1fe1566205716d65500d820bec2b23307da1866d
Ernest Marcinko <ernest.marcinko@gmail.com>
2024-03-28 11:48:30 +0000

    feat: added $message argument to expectCatchException
    The argument order has been changed.

--- src/MockUtils.php
+++ src/MockUtils.php
@@ -2,19 +2,27 @@
 
 namespace ErnestMarcinko\MockUtils;
 
-use ErrorException;
 use Exception;
-use InvalidArgumentException;
+use PHPUnit\Framework\AssertionFailedError;
 use Throwable;
 
+/**
+ * Mock utility methods for PHPUnit PHPUnit\Framework\TestCase
+ *
+ * This trait should be only used in PHPUnit\Framework\TestCase and descendants.
+ */
 trait MockUtils {
 	protected string $mock_namespace = __NAMESPACE__;
 
 	/**
-	 * Defines the global mocks via an array of function_name=>reponse
+	 * Defines the global mocks via an array of function_name=>reponse|callable
 	 *
-	 * @param array<string, mixed> $global_mocks key as function name, value as response
-	 * @param string|null $namespace the function namespace
+	 * Note, that this only works if the tested code is in a Namespace. Global functions in
+	 * Global namespace can't be mocked. The $namespace parameter should be the actual code namespace,
+	 * not the test code namespace.
+	 *
+	 * @param array<string, mixed|callable> $global_mocks key as function name, value as response value or callable
+	 * @param string|null $namespace the tested code namespace
 	 * @return void
 	 */
 	protected function setGlobalMocks(array $global_mocks, ?string $namespace): void {
@@ -45,22 +53,32 @@ trait MockUtils {
 	}
 
 	/**
-	 * @param class-string<Throwable> $throwable
-	 * @param callable $fn
+	 * Checks if the exception was thrown without execution termination
+	 *
+	 * Compared to TestCase::expectException this function will not terminate the
+	 * test execution. The function to test must be however passed in as a closure,
+	 * ex.: expectCatchException(fn()=>$o->myMethod(), ...)
+	 *
+	 * @param callable $fn Callable function, use a closure like fn()=>your_method()
+	 * @param class-string<Throwable> $throwable Throwable class
+	 * @param string|null $message Message to compare to, not required
 	 * @return void
-	 * @throws ErrorException
+	 * @throws AssertionFailedError
 	 */
-	protected function expectCatchException(string $throwable, callable $fn): void {
+	protected function expectCatchException(callable $fn, string $throwable, ?string $message = null): void {
 		if (!is_subclass_of( $throwable, Throwable::class)) {
-			throw new InvalidArgumentException("Class $throwable, is not a subclass of " . Throwable::class);
+			$this->fail("Class $throwable, is not a subclass of " . Throwable::class);
 		}
 		try {
 			call_user_func($fn);
 		} catch (Throwable $e) {
 			$this->assertSame($throwable, get_class($e));
+			if (isset($message)) {
+				$this->assertSame($message, $e->getMessage());
+			}
 		}
 		if (!isset($e)) {
-			throw new ErrorException("Expected to throw $throwable, but no exceptions were thrown.");
+			$this->fail("Expected to throw $throwable, but no exceptions were thrown.");
 		}
 	}
 }
--- src/MockUtilsTestCase.php
+++ src/MockUtilsTestCase.php
@@ -4,6 +4,9 @@ namespace ErnestMarcinko\MockUtils;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * A test class using the MockUtils, if you prefer this over traits
+ */
 class MockUtilsTestCase extends TestCase {
 	use MockUtils;
 }
